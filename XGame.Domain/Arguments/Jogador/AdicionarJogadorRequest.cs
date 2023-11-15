d8cb21d180eddf87cf917062d7d59d8a6be04545
Henrique Albano da Silva Nunes <henrique_albano@live.com>
2023-11-15 16:43:45 -0300

    Criando Value Objects, Enum, Arguments, Services e Interfaces.

---
+++ XGame.Domain/Arguments/Jogador/AdicionarJogadorRequest.cs
@@ -0,0 +1,15 @@
+﻿using XGame.Domain.Interface.Arguments;
+using XGame.Domain.ValueObjects;
+
+namespace XGame.Domain.Arguments.Jogador
+{
+    public class AdicionarJogadorRequest : IRequest
+    {
+        public Nome Nome { get; set; }
+
+        public Email Email { get; set; }
+
+        public string Senha { get; set; }
+
+    }
+}
---
+++ XGame.Domain/Interface/Services/IServiceJogador.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using XGame.Domain.Arguments.Jogador;
+
+namespace XGame.Domain.Interface.Services
+{
+    public interface IServiceJogador
+    {
+        AutenticarJogadorResponse AutenticarJogador(AutenticarJogadorRequest request);
+
+        AdicionarJogadorResponse AdicionarJogador(AdicionarJogadorRequest request);
+
+    }
+}
